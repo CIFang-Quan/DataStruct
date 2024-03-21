5e80f2c9bf7328c610b9c238b3ac305cff6b943a
Konata <78149883+CIFang-Quan@users.noreply.github.com>
2024-03-21 15:52:37 +0800

    Add files via upload

---
+++ list/list.c
@@ -0,0 +1,109 @@
+#include "list.h"
+#include <stdlib.h>
+#include "fatal.h"
+
+struct Node
+{
+    ElementType Element;
+    Position Next;
+};
+
+List MakeEmpty(List L)
+{
+    if (L != NULL){
+        DeleteList(L);
+    }
+    L = malloc(sizeof(struct Node));
+    if (L == NULL){
+        FatalError("Out of memory!");
+    }
+    L->Next = NULL;
+    return L;
+}
+
+int IsEmpty(List L)
+{
+    return L->Next == NULL;
+}
+
+int IsLast(Position P, List L)
+{
+    return P->Next == NULL;
+}
+
+Position Find(ElementType X, List L)
+{
+    Position P;
+    P = L->Next;
+    while (P != NULL && P->Element != X){
+        P = P->Next;
+    }
+    return P;
+}
+
+void Delete(ElementType X, List L)
+{
+    Position P, TmpCell;
+    P = FindPrevious(X, L);
+    if (!IsLast(P, L))
+    {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
+}
+
+Position FindPrevious(ElementType X, List L)
+{
+    Position P;
+    P = L;
+    while (P->Next != NULL && P->Next->Element != X){
+        P = P->Next;
+    }
+    return P;
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+    Position TmpCell;
+    TmpCell = malloc(sizeof(struct Node));
+    if (TmpCell == NULL){
+        FatalError("Out of space!!!");
+    }
+    TmpCell->Element = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
+}
+
+void DeleteList(List L)
+{
+    Position P, Tmp;
+    P = L->Next;
+    L->Next = NULL;
+    while (P != NULL)
+    {
+        Tmp = P->Next;
+        free(P);
+        P = Tmp;
+    }
+}
+
+Position Header(List L)
+{
+    return L;
+}
+
+Position First(List L)
+{
+    return L->Next;
+}
+
+Position Advance(Position P)
+{
+    return P->Next;
+}
+
+ElementType Retrieve(Position P)
+{
+    return P->Element;
+}
